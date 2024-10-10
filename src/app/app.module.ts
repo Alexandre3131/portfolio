9868be34994aef1d8afa0e688c7443f8ea6754e7
Benachir Alexandre <alexandre.benachir@gmail.com>
2024-10-10 19:36:40 +0200

    Page accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router'; // Import du RouterModule
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
-import { AppComponent } from './app.component'; // Assure-toi que le chemin est correct
-import { AccueilComponent } from './accueil/accueil.component';
 
 // Importation des routes
 import { routes } from './app.routes'; // Assure-toi que le chemin est correct
@@ -15,9 +11,7 @@ import { routes } from './app.routes'; // Assure-toi que le chemin est correct
     BrowserModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
-  ],
+  providers: [],
   bootstrap: []
 })
 export class AppModule {}
