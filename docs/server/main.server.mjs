0a86dd9dfe210f1362a34145f0b7d0bcc39bdeb2
Benachir Alexandre <alexandre.benachir@gmail.com>
2024-10-10 19:04:10 +0200

    Page accueil

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZAXRRXHQ.mjs";import"./chunk-6F2JFXXC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-DA5QIHN5.mjs";import"./chunk-HRZA6QCY.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
