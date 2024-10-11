849e7377242063cdb1dfc8bcc35bef282326ed78
Benachir Alexandre <alexandre.benachir@gmail.com>
2024-10-11 21:58:35 +0200

    Ajout menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 
